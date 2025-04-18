f20be71121c2ffa23d57bddbba2c4f00e7f2c20b
Ben Upton <bupton937@lsr7.net>
2025-04-18 15:49:14 -0500

    Remove .env and _old/ from tracking

--- _old/check_status.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-echo isset($_SESSION['active']) ? $_SESSION['active'] : 'false';
-?>
--- _old/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: index.php"); // Redirects back to the login page
-exit;
-?>
